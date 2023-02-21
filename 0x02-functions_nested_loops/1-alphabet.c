7027d332cccd5e451441b7ea49628fe158db4935
Your Name <you@example.com>
2023-02-21 15:20:34 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*print_alphabet - Prints the alphabets in lowercase, followed by a new line.
+*/
+void print_alphabet(void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+_putchar(lc);
+_putchar('\n');
+}
