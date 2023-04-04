28462462f7fb2b7edbab11c306de260082350513
Your Name <you@example.com>
2023-04-04 04:21:44 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,13 +7,13 @@
 */
 int pop_listint(listint_t **head)
 {
-listint_t *popped;
-int content;
-if (*head == NULL)
+listint_t *temp;
+int num;
+if (!head || !*head)
 return (0);
-popped = *head;
-content = popped->n;
-free(popped);
-*head = (*head)->next;
-return (content);
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
 }
